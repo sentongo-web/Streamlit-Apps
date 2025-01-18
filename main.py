ab2dc9c890d4b7c498120b8d8c65f4a74a2446bf
sentongo-web <sentongogray1992@gmail.com>
2025-01-18 18:52:10 +0300

    ok

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.text("Hello streamlit- We are learning streamlit")
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.markdown("This is a markdown")
+st.markdown("#This is a markdown1")
+st.markdown("##This is a markdown2")
+st.markdown("###This is a markdown3")
