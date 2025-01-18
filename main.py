30c8595fd0ba97e9ee322bad574f6d78b2de7314
sentongo-web <sentongogray1992@gmail.com>
2025-01-18 19:09:04 +0300

    ok

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import streamlit as st
-st.text("Hello streamlit- We are learning streamlit")
-st.header("This is a header")
-st.subheader("This is a subheader")
-st.markdown("This is a markdown")
-st.markdown("#This is a markdown1")
-st.markdown("##This is a markdown2")
-st.markdown("###This is a markdown3")
+import pandas as pd
+# st.text("Hello streamlit- We are learning streamlit")
+# st.header("This is a header")
+# st.subheader("This is a subheader")
+# st.markdown("This is a markdown")
+# st.markdown("#This is a markdown1")
+# st.markdown("##This is a markdown2")
+# st.markdown("###This is a markdown3")
+# st.caption("This is a caption")
+# st.code("import pandas")
