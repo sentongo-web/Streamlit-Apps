79127859e49916d069c8d243f15050dbbb8e8f3e
sentongo-web <sentongogray1992@gmail.com>
2025-01-18 19:32:13 +0300

    ok

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import streamlit as st
+import pandas as pd
+df = pd.read_csv("patient_data.csv")
+st.dataframe(df)
+
+# line charts
+st.line_chart(df, x="AdmissionYear", y= ["VisitCount", "Age"])
+
+###area charts
+st.area_chart(df, x="AdmissionYear", y= ["VisitCount", "Age"])
+
+###bar charts
+st.bar_chart(df, x="AdmissionYear", y= ["VisitCount", "Age"])
+
+##streamlit map
+# Load the dataset
+map_data = pd.read_csv("map_data.csv")
+st.map(map_data)
+# Display the map
+
+# st.table(df)
 
 # st.text("Hello streamlit- We are learning streamlit")
 # st.header("This is a header")
